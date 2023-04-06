4f7c4b58fae25df5e2814b29563f439460d1f215
AhmadWagih <ahmwageeh@gmail.com>
2023-04-06 21:54:30 +0200

    mm

--- main.py
+++ main.py
@@ -3,26 +3,26 @@ from extract import *
 from pydantic import BaseModel
 import os
 
-api = FastAPI()
 SECRET = os.getenv("SECRET")
+app = FastAPI()
 
 class message(BaseModel):
     msg:str
     secret:str
 
 
-@api.get("/")
+@app.get("/")
 async def get():
     return {"message : successfully"}
 
-@api.get("/homepage")
+@app.get("/homepage")
 async def homepage():
     driver = createDriver()
     html = getGoogleHomePage(driver)
     driver.close()
     return html
 
-@api.post("/dopgwork")
+@app.post("/dopgwork")
 async def dopgwork(mes = message,backgroundTasks=BackgroundTasks):
     doBGWork(mes)
     print("successful")
